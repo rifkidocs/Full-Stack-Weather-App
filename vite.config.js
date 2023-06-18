0e27ec7431d3bd69e4a1272341381b83e87480eb
Muhammad Rifki Ardiansah <rifkiars@gmail.com>
2023-06-18 13:53:44 +0700

    fix routing client side

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import reactRouter from "@vitejs/plugin-react-router";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    middlewareMode: "html",
-  },
+  plugins: [react(), reactRouter()],
 });
